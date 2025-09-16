fcaa9017e2547859e816e999dbffa8cf199d6b6d
issamabchri-spec <emranabchri@gmail.com>
2025-09-16 15:45:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let age = 20;
+
+if (age < 18) {
+ console.log("vous etes mineur.");   
+}else if (age === 18){
+ console.log("vous venez d'atteindre la majarote !");   
+} else {
+ console.log("vous etes majeur.");   
+}
